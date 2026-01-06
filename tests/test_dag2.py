1c80fce38c96d4d5db9e3e6c881bf28d0a137e72
Divya Eeli <119840639+22A91A61E8@users.noreply.github.com>
2026-01-06 15:55:58 +0530

    Add test_dag2.py unit tests

---
+++ tests/test_dag2.py
@@ -0,0 +1,27 @@
+# tests/test_dag2.py
+from airflow.models import DagBag
+import pytest
+
+def test_dag2_loaded():
+    dagbag = DagBag(dag_folder='dags/', include_examples=False)
+    assert 'data_transformation_pipeline' in dagbag.dags
+    assert len(dagbag.import_errors) == 0
+
+def test_dag2_structure():
+    dagbag = DagBag(dag_folder='dags/', include_examples=False)
+    dag = dagbag.dags['data_transformation_pipeline']
+    assert len(dag.tasks) == 2
+
+def test_dag2_task_dependencies():
+    dagbag = DagBag(dag_folder='dags/', include_examples=False)
+    dag = dagbag.dags['data_transformation_pipeline']
+    
+    create_task = dag.get_task('create_transformed_table')
+    transform_task = dag.get_task('transform_and_load')
+    
+    assert transform_task in create_task.downstream_list
+
+def test_dag2_schedule():
+    dagbag = DagBag(dag_folder='dags/', include_examples=False)
+    dag = dagbag.dags['data_transformation_pipeline']
+    assert dag.schedule_interval == '@daily'
